9065d888ba0d9dabec42f2747274c96144ed9f37
adolphlwq <kenan3015@gmail.com>
2017-08-21 20:46:35 +0800

    [backend] remove handlers in api

--- server/api.go
+++ server/api.go
@@ -3,27 +3,25 @@ package server
 import (
 	"time"
 
-	"github.com/adolphlwq/webim/service"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	im      *service.IMService
 	session sessions.Session
 )
 
 // WebIMAPI main api endpoint for webim
-func WebIMAPI(serviceUrl string, dbs *service.DBService, appService *ServiceProvider) {
-	router := gin.Default()
-	im = service.NewIMService(dbs)
-
+func WebIMAPI(serviceUrl string, appService *ServiceProvider) {
 	// use session
 	store := sessions.NewCookieStore([]byte("secret"))
 	store.Options(sessions.Options{
 		MaxAge: 30 * 60, //30mins
+		Path:   "/",
 	})
+
+	router := gin.Default()
 	router.Use(sessions.Sessions("webim-session", store))
 	// use cors
 	router.Use(cors.New(cors.Config{
@@ -41,28 +39,6 @@ func WebIMAPI(serviceUrl string, dbs *service.DBService, appService *ServiceProv
 
 	router.GET("/health", WrapeService(appService, HealthCheck))
 
-	userAPI := router.Group("/api/v1/user")
-	{
-		userAPI.POST("/login", UserLogin)
-		userAPI.POST("/register", UserRegister)
-		userAPI.POST("/logout", LoginOut)
-		userAPI.GET("/get", GetUserByName)
-	}
-
-	friendAPI := router.Group("/api/v1/friend")
-	{
-		friendAPI.POST("/add", AddFriendRelationship)
-		friendAPI.GET("/list", ListFriendRelationship)
-		friendAPI.PUT("/delete", DeleteFriendRelationship)
-	}
-
-	messageAPI := router.Group("/api/v1/message")
-	{
-		messageAPI.GET("/unread", GetUnreadMsg)
-		// add username(id) to path is inspired
-		// my deep thinking and https://github.com/gin-gonic/gin/issues/461
-		messageAPI.GET("/ws/:username", WSMsgHandler)
-	}
 	router.Run(serviceUrl)
 }
 
