3b116fde43c8ce5b31745c86bdbcf16fffd8302f
adolphlwq <kenan3015@gmail.com>
2017-08-22 01:43:01 +0800

    [backend] update response message

--- server/health_check.go
+++ server/health_check.go
@@ -9,7 +9,6 @@ import (
 // HealthCheck return status of chat server
 func HealthCheck(c *gin.Context, appService *ServiceProvider) {
 	c.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
 		"message": "health",
 	})
 }
