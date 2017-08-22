577b77587208269127931df0bece39f745340f2a
adolphlwq <kenan3015@gmail.com>
2017-08-22 10:56:59 +0800

    [backend] parse flag

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	flag.StringVar(&configPath, "config", "./default.properties", "config file path")
 	flag.StringVar(&h, "h", "0.0.0.0", "host server bind")
 	flag.StringVar(&p, "p", "9066", "port server bind")
+	flag.Parse()
 
 	addr := net.JoinHostPort(h, p)
 	mysqlClient := mysql.NewMySQLClient(configPath)
