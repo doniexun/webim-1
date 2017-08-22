017f322e4a4113aae3540798524a8fd79cf1bbcc
adolphlwq <kenan3015@gmail.com>
2017-08-22 11:53:24 +0800

    [backend] encryptpassword and test

--- util/util.go
+++ util/util.go
@@ -1,8 +1,26 @@
 package util
 
-import "time"
+import (
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"golang.org/x/crypto/scrypt"
+)
+
+const PASS_SALT = "WebIM@adolph*lwq!"
 
 // GetCurrentTimestamp return current utc timestamp
 func GetCurrentTimestamp() int64 {
 	return time.Now().UTC().Unix()
+}
+
+// EncryptPassword encrypt password
+func EncryptPassword(password string) string {
+	dk, err := scrypt.Key([]byte(password), []byte(PASS_SALT),
+		16384, 8, 1, 32)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	return string(dk)
 }
--- util/util_test.go
+++ util/util_test.go
@@ -8,3 +8,10 @@ func TestGetCurrentTimestamp(t *testing.T) {
 		t.Error()
 	}
 }
+
+func TestEncryptPassword(t *testing.T) {
+	s := EncryptPassword("test")
+	if len(s) <= 0 {
+		t.Errorf("test %s error", "EncryptPas")
+	}
+}
