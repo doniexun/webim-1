2f72849ef89022c218f0ce0672f9948e4c952c3b
adolphlwq <kenan3015@gmail.com>
2017-08-22 15:29:27 +0800

    [backend] update

--- server/constant_message.go
+++ server/constant_message.go
@@ -4,10 +4,12 @@ package server
 const (
 	ErrMoreUserInfo = "need more user info."
 	ErrUserExisted  = "user has existed in db."
+	ErrUserNotExisted  = "user does not exist in db."
 )
 
 // info message
 const (
 	InfoHealthCheck     = "health."
 	UserRegisterSuccess = "user register success."
+	UserLoginSuccess = "user login success."
 )
