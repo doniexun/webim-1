54c811e706088b3a5b1511f78973e9de4f8324bd
adolphlwq <kenan3015@gmail.com>
2017-08-22 14:13:28 +0800

    [backend] add test helper funcs

--- server/api_test.go
+++ server/api_test.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"testing"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/adolphlwq/webim/mysql"
 	"github.com/gin-gonic/gin"
 )
@@ -42,4 +47,25 @@ func startTestServer(t *testing.T) {
 
 	t.Logf("wait 2s for test server to start...\n")
 	time.Sleep(time.Second * 2)
+}
+
+func postJSON(postURL string, data io.Reader) map[string]interface{} {
+	resp, err := http.Post(postURL, "application/json", data)
+	if err != nil {
+		logrus.Fatalf("post data error: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Fatalf("read body of response error: %v", err)
+	}
+
+	var mp map[string]interface{}
+	err = json.Unmarshal(body, &mp)
+	if err != nil {
+		logrus.Fatalf("parse body of response error: %v", err)
+	}
+
+	return mp
 }
