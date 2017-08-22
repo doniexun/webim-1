2abca1901c1d36c1a231fbe841a86a9fa9acedd3
adolphlwq <kenan3015@gmail.com>
2017-08-22 19:25:56 +0800

    [backend] standarize and test

--- entity/contact.go
+++ entity/contact.go
@@ -1,6 +1,7 @@
 package entity
 
 import "time"
+import "strings"
 
 type Contact struct {
 	ID        int64     `gorm:"primary_key"`
@@ -8,4 +9,13 @@ type Contact struct {
 	FriendMax string    `json:"fmax"`
 	AddedTime time.Time `json:"added_time"`
 	State     string    `json:"state"`
+}
+
+// Standerize make friendMax and friendMin lexicographically
+func (c *Contact) Standerize() {
+	if strings.Compare(c.FriendMax, c.FriendMin) < 0 {
+		tmp := c.FriendMax
+		c.FriendMax = c.FriendMin
+		c.FriendMin = tmp
+	}
 }
