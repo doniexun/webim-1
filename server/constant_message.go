6ab2ab632db84f6ff51346420fc2bbe24b0a930a
adolphlwq <kenan3015@gmail.com>
2017-08-22 17:05:51 +0800

    [backend] update constant messages

--- server/constant_message.go
+++ server/constant_message.go
@@ -5,6 +5,7 @@ const (
 	ErrMoreUserInfo = "need more user info."
 	ErrUserExisted  = "user has existed in db."
 	ErrUserNotExisted  = "user does not exist in db."
+	ErrInternel = "server intervel error."
 )
 
 // info message
@@ -12,4 +13,5 @@ const (
 	InfoHealthCheck     = "health."
 	UserRegisterSuccess = "user register success."
 	UserLoginSuccess = "user login success."
+	UserListSuccess = "users list."
 )
