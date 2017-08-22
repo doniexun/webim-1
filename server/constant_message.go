9ae6be21e5a2ded0f5f3ef54a5314e9707d8e145
adolphlwq <kenan3015@gmail.com>
2017-08-22 14:12:05 +0800

    [backend] add new comstant msgs

--- server/constant_message.go
+++ server/constant_message.go
@@ -3,9 +3,11 @@ package server
 // error message
 const (
 	ErrMoreUserInfo = "need more user info."
+	ErrUserExisted  = "user has existed in db."
 )
 
 // info message
 const (
-	InfoHealthCheck = "health."
+	InfoHealthCheck     = "health."
+	UserRegisterSuccess = "user register success."
 )
