e36b3223cd9c2b84dd604842f76a34653c22ba18
adolphlwq <kenan3015@gmail.com>
2017-08-23 09:43:01 +0800

    [backend] update format

--- server/constant_message.go
+++ server/constant_message.go
@@ -2,20 +2,22 @@ package server
 
 // error message
 const (
-	ErrMoreUserInfo = "need more user info."
-	ErrUserExisted  = "user has existed in db."
-	ErrUserNotExisted  = "user does not exist in db."
-	ErrInternel = "server intervel error."
-	ErrMoreContactInfo = "need more contact info."
-	ErrContactRealitionshipExisted = "contact relationship has been existed."
-	ErrUsernameNotExistedTmpl = "username %s does not exist."
+	ErrMoreUserInfo                   = "need more user info."
+	ErrUserExisted                    = "user has existed in db."
+	ErrUserNotExisted                 = "user does not exist in db."
+	ErrInternel                       = "server intervel error."
+	ErrMoreContactInfo                = "need more contact info."
+	ErrContactRealitionshipExisted    = "contact relationship has been existed."
+	ErrUsernameNotExistedTmpl         = "username %s does not exist."
+	ErrContactRealitionshipNotExisted = "contact relationship does not exist."
 )
 
 // info message
 const (
-	InfoHealthCheck     = "health."
-	UserRegisterSuccess = "user register success."
-	UserLoginSuccess = "user login success."
-	UserListSuccess = "users list."
-	ContactAddSuccess = "contact relationship add success."
+	InfoHealthCheck      = "health."
+	UserRegisterSuccess  = "user register success."
+	UserLoginSuccess     = "user login success."
+	UserListSuccess      = "users list."
+	ContactAddSuccess    = "contact relationship add success."
+	ContactDeleteSuccess = "contact relationship delete success."
 )
