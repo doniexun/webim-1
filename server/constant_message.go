86c57e7c9c8e8277c6b8e9b789a3d9edbc469f51
adolphlwq <kenan3015@gmail.com>
2017-08-27 16:39:31 +0800

    update

--- server/constant_message.go
+++ server/constant_message.go
@@ -10,6 +10,13 @@ const (
 	ErrContactRealitionshipExisted    = "contact relationship has been existed."
 	ErrUsernameNotExistedTmpl         = "username %s does not exist."
 	ErrContactRealitionshipNotExisted = "contact relationship does not exist."
+	ErrNeedUserToken                  = "need user token."
+	ErrJWTInvalid                     = "user jwt token invalid."
+	ErrWebSocketUpgrade               = "upgrade to websocket protocal error."
+	ErrUserWebSocketExisted           = "user websocket has existed."
+	ErrReadMessageFromWebSocket       = "read message from websocket error."
+	ErrWriteMessageToReveiver         = "write message to receiver error."
+	ErrParamInPath                    = "param %s in request path error."
 )
 
 // info message
@@ -22,4 +29,5 @@ const (
 	ContactDeleteSuccess           = "contact relationship delete success."
 	ContactRelationshipIsInactive  = "contact relationship is already inactive, do nothing."
 	ContactRelationshipListSuccess = "contact relationship list."
+	GetUnreadMessageSuccess        = "get unread messages success."
 )
