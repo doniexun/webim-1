6cd4e0585dd3c1cec0622606b61a98cf8af30e94
adolphlwq <kenan3015@gmail.com>
2017-08-27 14:46:27 +0800

    [backend] add websocket upgrader

--- server/service_provider.go
+++ server/service_provider.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"github.com/adolphlwq/webim/mysql"
+	"github.com/gorilla/websocket"
 )
 
 // ServiceProvider object hold service which server need
 type ServiceProvider struct {
-	MysqlClient *mysql.Client
+	MysqlClient       *mysql.Client
+	WebSocketUpgrader *websocket.Upgrader
 }
